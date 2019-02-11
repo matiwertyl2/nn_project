0223ffe3fd87cd66d8b3408ae24941f7ccec9e88
matiwertyl2 <mateuszhazy@gmail.com>
2019-02-11 18:59:38 +0100

    Change K-means to position invariant

--- k-means.py
+++ k-means.py
@@ -3,23 +3,21 @@ import numpy as np
 def iou(boxes, centers):
     # box [xmin, xmax, ymin, ymax]
     # center tak samo
-    leftX = np.maximum(boxes[:, 0].reshape((-1, 1)), centers.T[0, :])
-    rightX = np.minimum(boxes[:, 1].reshape((-1, 1)), centers.T[1, :])
-    topY = np.maximum(boxes[:, 2].reshape((-1, 1)), centers.T[2, :])
-    bottomY = np.minimum(boxes[:, 3].reshape((-1, 1)), centers.T[3, :])
 
-    interX = np.maximum(0, rightX - leftX)
-    interY = np.maximum(0, bottomY - topY)
+    interX = np.minimum(boxes[:, 0].reshape((-1, 1)), centers.T[0, :])
+    interY = np.minimum(boxes[:, 1].reshape((-1, 1)), centers.T[1, :])
 
     interS = interX * interY 
 
-    boxesS = (boxes[:, 1] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 2])
+    boxesS = (boxes[:, 1] * boxes[:, 0])
 
-    centersS = (centers[:, 1] - centers[:, 0]) * (centers[:, 3] - centers[:, 2]) 
+    centersS = (centers[:, 1] * centers[:, 0])
 
     unionS = (boxesS.reshape((-1, 1)) + centersS.T) - interS
 
-    return interS / unionS
+    print np.min(unionS)
+    print np.min(boxesS), np.argmin(boxesS)
+    return interS / (unionS + 0.00000001)
 
 
 
@@ -36,11 +34,15 @@ def k_means(max_iter, idle_value, K, boxes):
         prev_error = error
         for k in range(K):
             assigned_boxes = np.argwhere(assigned_centers == k).flatten()
-            centers[k] = boxes[assigned_boxes].mean(axis=0)
+            if len(assigned_boxes) > 0:
+                centers[k] = boxes[assigned_boxes].mean(axis=0)
     return centers
 
 
 
 dogs_metadata = np.array([map(float, line.split(',')) for line in open('metadata.csv').read().splitlines()])[:, 1:]
+boxes = np.zeros((dogs_metadata.shape[0], 2))
+boxes[:, 0] = dogs_metadata[:, 1] - dogs_metadata[:, 0]
+boxes[:, 1] = dogs_metadata[:, 3] - dogs_metadata[:, 2]
 
-k_means(1000, 1e-6, 100, dogs_metadata)
+print k_means(1000, 1e-6, 5, boxes)
